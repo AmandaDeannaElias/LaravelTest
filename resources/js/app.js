b3fe9a584e3db0310a9239d41ebd096c6139e025
AmandaDeannaElias <amanda.elias@live.ca>
2021-09-25 21:58:10 -0400

    ran npm install, created my file to implement vue, changed my route to interact with view instead of controller

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('front-page', require('./components/front.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/front.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <h1 class="my-4">My Laravel Test</h1>
+                <div class="list-group">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
